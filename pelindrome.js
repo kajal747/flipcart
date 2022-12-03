4470a91ce94e3db5a1926f0b1f7829a8c34f6b68
Your Name <you@example.com>
2022-12-03 15:33:38 +0530

    incorrect code

---
+++ pelindrome.js
@@ -0,0 +1,14 @@
+let s="aman";
+
+function reverse(){
+    let bag="";
+    for(let i=str.length-1;i>=0;i--){
+        bag=bag+str[i];
+    }
+    return bag;
+}
+let rev_str=reverse(str);
+if(str=rev_str){
+  console.log("palindrome")
+}else{
+  console.log("NA")
